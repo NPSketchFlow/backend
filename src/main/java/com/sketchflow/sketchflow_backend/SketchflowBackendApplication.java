897d6b87dc8ec025c3871a4aaf2223c803f0248e
Kasun Shakthi <2001shakthikasun@gmail.com>
2025-10-27 21:47:17 +0530

    first commit

---
+++ src/main/java/com/sketchflow/sketchflow_backend/SketchflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sketchflow.sketchflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sketchflow/sketchflow_backend/SketchflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sketchflow.sketchflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
